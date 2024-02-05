1a26d73f3184d7b5f9c8c7fe0e336e92c571484a
xulllion024 <153744780+xulllion024@users.noreply.github.com>
2024-02-05 16:59:52 +0300

    Update mm.php

--- mm.php
+++ mm.php
@@ -9,7 +9,7 @@ session_start(ss2);
 <?php
 // Set session variables
 $_SESSION["favcolor"] = "go";
-$_SESSION["favanimal"] = "cawool2";
+$_SESSION["favanimal"] = "cawol2";
 echo "Session variables are set.";
 ?>
 
